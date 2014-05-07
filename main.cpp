e9942a3fff1acc61499424e4370b84b910febda5
AlexSol <faranheit0@gmail.com>
2014-05-07 21:49:15 +0300

    no message

--- main.cpp
+++ main.cpp
@@ -7,19 +7,29 @@
 
 using namespace std;
 
+struct setting
+    {
+        bool MakeBatFile;
+        bool FileCutNumber;
+        bool FileCutSize;
+        unsigned int NumberFile;
+        unsigned __int64 SizeFile;
+    } setup;
+
 void argError(const char* arg);
 void printError(int errorCode);
 BOOL DirectoryExists(const char* dirName);
+bool askNumber(const char*);
+bool Commandline(int argc, char **argv);
 
 int main(int argc, char **argv)
 {
-    char parametertTextCut[]  = "-c";
-    char parametertTextJoin[] = "-j";
-    char parametertTextSize[] = "-s";
-
+    Commandline(argc, argv);
+    return 0;
+/*
     if( argc >= 4){
-        if ( strcmp(parametertTextCut, argv[1]) == NULL ){
-            if( strcmp(parametertTextSize, argv[2]) == NULL ){
+        if ( strcmp(ParametertCutText, argv[1]) == NULL ){
+            if( strcmp(ParametertSizeText, argv[2]) == NULL ){
 
                 if( atoi(argv[3]) == 0 ){ //size outFile
                     cout<< "outFile does not have zero size" << endl;
@@ -47,7 +57,7 @@ int main(int argc, char **argv)
             }
 
         }else{
-            if( strcmp(parametertTextJoin, argv[1]) == NULL){
+            if( strcmp(ParametertCreateBatText, argv[1]) == NULL){
                     cout<< "test ver. " << endl;
             }else{
                 argError(argv[1]);
@@ -55,8 +65,7 @@ int main(int argc, char **argv)
             }
         }
     }
-
-    return 0;
+*/
     if(argc == 4){
         char* inFileName = argv[1];
         char* ouFileName = argv[2];
@@ -155,4 +164,95 @@ BOOL DirectoryExists(const char* dirName) {
         return false;
       }
       return (attribs & FILE_ATTRIBUTE_DIRECTORY);
+}
+
+bool askNumber(const char* str)
+{
+    if( *str >= '1' && *str <= '9' ){
+            return true;
+    }
+
+    return false;
+}
+
+bool Commandline(int argc, char **argv)
+{
+    char ParMakeBat[]   = "-b";
+    char ParSize[]      = "-s";
+    char ParNumber[]    = "-n";
+    //char ParametertCutText[]       = "-c";
+
+    char byte[]     = "b";
+    char kilobyte[] = "kb";
+    char megabyte[] = "mb";
+    char gigabyte[] = "gb";
+    //char terabyte[] = "tb";
+
+    if( argc == 1){
+        cout<< "not parameterts" << endl;
+        return true;
+    }
+
+    for( int i = 1; i<argc; i++)
+    {
+        if( strcmp(argv[i], ParMakeBat) == NULL ){ setup.MakeBatFile = true;};
+
+        if( strcmp(argv[i], ParNumber) == NULL ){
+            int number = atoi(argv[i+1]);
+            if( number != 0 ){
+                setup.FileCutNumber = true;
+                setup.NumberFile = number;
+                cout<< setup.NumberFile << endl;
+                i++;
+            }else{
+                cout<< "invalid number file - "<< argv[i+1]<< endl;;
+            }
+        }
+
+        if( strcmp(argv[i], ParSize) == NULL ){
+            int nsize = atoi(argv[i+1]);
+            if( nsize != 0){
+                setup.FileCutSize = true;
+                setup.SizeFile = nsize;
+                for(int j = 1; j<strlen(argv[i+1]); j++)
+                {
+                    if( argv[i+1][j] >= 'A'){
+                            if( argv[i+1][j] == 'b' ){
+                                //setup.SizeFile = nsize;
+                                cout<< byte << endl;
+                                break;
+                            }
+                            if( argv[i+1][j] == 'k' && argv[i+1][j+1] == 'b' ){
+                                setup.SizeFile <<= 10;
+                                cout<< kilobyte << endl;
+                                break;
+                            }
+                            if( argv[i+1][j] == 'm' && argv[i+1][j+1] == 'b' ){
+                                setup.SizeFile <<= 20;
+                                cout<< megabyte << endl;
+                                break;
+                            }
+                            if( argv[i+1][j] == 'g' && argv[i+1][j+1] == 'b' ){
+                                setup.SizeFile <<= 30;
+                                cout<< gigabyte << endl;
+                                break;
+                            }
+                           /* if( argv[i+1][j] == 't' && argv[i+1][j+1] == 'b' ){
+                                setup.SizeFile <<= 40;
+                                cout<< gigabyte << endl;
+                                break;
+                           }
+                           */
+                            cout<< "invalid storage unit - "<< argv[i+1] << argv[i+2] << endl;
+                            break;
+                    }
+                }
+                i++;
+            }else{
+                cout<< "invalid size - " << argv[i+1] << endl;
+            }
+        }
+
+       cout<< setup.SizeFile << endl;
+    }
 }
